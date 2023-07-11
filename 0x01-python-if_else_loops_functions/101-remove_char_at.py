9a45d25e921df84f17b1185e9a3e89b6d9bd5b5f
Btris04 <ifebeatrice55@gmail.com>
2023-07-11 05:38:04 -0700

    task 2

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,6 +2,6 @@
 # 101-remove_char_at.py
 
 def remove_char_at(str, n):
- if n < 0:
-  return (str)
- return (str[:n] + str[n+1:])
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
